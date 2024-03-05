48660c5e271e650839fc6f7a405c4a6afd871348
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-03-05 23:24:33 +0100

    DataSource

--- app/src/main/java/com/example/ecommerceui/data/DataSource.kt
+++ app/src/main/java/com/example/ecommerceui/data/DataSource.kt
@@ -7,6 +7,9 @@ object DataSource {
         Category(R.drawable.vector, R.string.category_name_1),
         Category(R.drawable.vector1, R.string.category_name_2),
         Category(R.drawable.vector2, R.string.category_name_3),
+        Category(R.drawable.vector3, R.string.category_name_4),
+        Category(R.drawable.vector1, R.string.category_name_2),
+        Category(R.drawable.vector2, R.string.category_name_3),
         Category(R.drawable.vector3, R.string.category_name_4)
     )
 }
