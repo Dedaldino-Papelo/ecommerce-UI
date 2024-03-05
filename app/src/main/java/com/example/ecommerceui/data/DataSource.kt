c81ed2ed59a57fd167472126ed873e3869e39447
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-03-05 23:41:01 +0100

    fix

--- app/src/main/java/com/example/ecommerceui/data/DataSource.kt
+++ app/src/main/java/com/example/ecommerceui/data/DataSource.kt
@@ -8,8 +8,5 @@ object DataSource {
         Category(R.drawable.vector1, R.string.category_name_2),
         Category(R.drawable.vector2, R.string.category_name_3),
         Category(R.drawable.vector3, R.string.category_name_4),
-        Category(R.drawable.vector1, R.string.category_name_2),
-        Category(R.drawable.vector2, R.string.category_name_3),
-        Category(R.drawable.vector3, R.string.category_name_4)
     )
 }
